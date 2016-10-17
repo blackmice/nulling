95c7cc78eb1069230c326a9219e61f6546c13905
QNL Workstation 2 <QNL Workstation 2>
2016-10-17 14:53:34 -0700

    class definition

--- dc_control.py
+++ dc_control.py
@@ -4,16 +4,18 @@ class dc_control:
 		import pyvisa
 		rm = pyvisa.ResourceManager()
 		self.dev = rm.get_instrument('ASRL4::INSTR',read_termination='\r', write_termination='\r', baud_rate=115200)
-		idn = dev.query('IDN')
+		idn = self.dev.query('IDN')
 		self.dev_name = idn[0:6];
 		
 	def setVoltage(self,channel, voltage):
-	if channel <10:
-		channel_id = 'CH0'+ str(channel)+' ';
-	else :
-		channel_id = 'CH' + str(channel)+' ';
+		if channel <10:
+			channel_id = 'CH0'+ str(channel)+' ';
+		else :
+			channel_id = 'CH' + str(channel)+' ';
 
-	voltage_s = (voltage+100)/200.0; 
-	str_v = "{:7.5f}".format(voltage_s)
-	self.dev.query(dev_name+channel_id+str_v);
-	return None
+		voltage_s = (voltage+100)/200.0; 
+		str_v = "{:7.5f}".format(voltage_s)
+		self.dev.query(self.dev_name+channel_id+str_v);
+		return None
+
+
