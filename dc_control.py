56c6c16109b85e1b14f0ee878d88467659efe232
John Luo <HELLOWORLD@Johns-MacBook-Pro-6.local>
2016-10-17 14:35:50 -0700

    class definition

--- dc_control.py
+++ dc_control.py
@@ -1,19 +1,13 @@
+class dc_control:
 
-
-
-
-
-def connect_Stahl(devName ='ASRL4::INSTR'):
-	import pyvisa
-	rm = pyvisa.ResourceManager()
-	dev = rm.get_instrument('ASRL4::INSTR',read_termination='\r', write_termination='\r', baud_rate=115200)
-	idn = dev.query('IDN')
-	id_dev = idn[0:6];
-	return (dev,id_dev)
-
-
-
-def setVoltage(channel, voltage, dev, dev_name):
+	def __init__(self,devName ='ASRL4::INSTR'):
+		import pyvisa
+		rm = pyvisa.ResourceManager()
+		self.dev = rm.get_instrument('ASRL4::INSTR',read_termination='\r', write_termination='\r', baud_rate=115200)
+		idn = dev.query('IDN')
+		self.dev_name = idn[0:6];
+		
+	def setVoltage(self,channel, voltage):
 	if channel <10:
 		channel_id = 'CH0'+ str(channel)+' ';
 	else :
@@ -21,15 +15,5 @@ def setVoltage(channel, voltage, dev, dev_name):
 
 	voltage_s = (voltage+100)/200.0; 
 	str_v = "{:7.5f}".format(voltage_s)
-	dev.query(dev_name+channel_id+str_v);
+	self.dev.query(dev_name+channel_id+str_v);
 	return None
-
-
-# method call
-temp = connect_Stahl()
-dev = temp[0]
-dev_name = temp[1]
-
-
-# voltage goes from -100mV to 100mV
-setVoltage(1,50,dev,dev_name)
